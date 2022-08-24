1823415e120144ef26980bc642e18ce51bb023ed
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-25 02:30:03 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Code!
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const eventName = 'birthday';
+function writeCards(names, eventName) {
+    let arrNames = [];
+    for(let i = 0; i < names.length; i++) {
+        arrNames.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return arrNames;
+}
+
+
+function countDown(num) {
+    for(let i=num; i>=0; i--) {
+        console.log(i)
+    } 
+}
+console.log(countDown(11));
